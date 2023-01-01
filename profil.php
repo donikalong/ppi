dfa8a76dfccd976d03ff727570979c58ad8f073e
Doni Romdoni <58791550+donikalong@users.noreply.github.com>
2023-01-01 20:19:13 +0700

    Add files via upload

---
+++ profil.php
@@ -0,0 +1,138 @@
+<div class="slide-item overlay" style="background-image: url('images/header.jpeg')">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 align-self-center">
+            <h1 class="heading mb-5">Periksa Kesehatan Kamu dengan kami</h1>
+            <p><a href="#" class="btn btn-primary">Get started</a></p>
+          </div>
+        </div>
+      </div>  
+</div>
+
+<div class="container quick-contact">
+      <div class="row">
+        <div class="col-lg-4">
+          <a href="#" class="link-lg d-flex align-items-center">
+            <span class="icon-phone"></span>
+            <div>
+              <span>Give us a call</span>
+              <strong>0721 78954 1579</strong>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4">
+          <a href="#" class="link-lg d-flex align-items-center">
+            <span class="icon-envelope"></span>
+            <div>
+              <span>Send us a message</span>
+              <strong>admin@klinikteluksehatnedika.site</strong>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4">
+          <a href="#" class="link-lg d-flex align-items-center">
+            <span class="icon-room"></span>
+            <div>
+              <span>Visit us</span>
+              <strong>2918 Fake Street</strong>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>    
+<div class="site-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <p><img src="images/13.jpg" alt="Image" class="img-fluid"></p>
+          </div>
+          <div class="col-lg-5 ml-auto">
+            <span class="subheading">About Us</span>
+            <h2 class="heading"><strong class="text-primary">We Are Happy To Serve You!</strong></h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, neque, dolorem. Iusto dolore omnis ex vero consequatur et deserunt officia incidunt at illum corrupti adipisci consectetur, veniam veritatis? Neque, cupiditate.</p>
+            <p><a href="#" class="btn btn-primary">Contact Us</a> <a href="#" class="btn btn-secondary">Read More</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section bg-1">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-5 mr-auto">
+            <span class="subheading">Our Mission</span>
+            <h2 class="heading">We Provide <strong class="text-primary">High Solutions</strong> for Your Health</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, neque, dolorem. Iusto dolore omnis ex vero consequatur et deserunt officia incidunt.</p>
+            
+          </div>
+          <div class="col-lg-6">
+            <figure class="video-image">
+              <a href="https://www.youtube.com/watch?v=vSndrIBTDUw" data-fancybox class="btn-play"><span class="icon-play"></span></a>
+              <img src="images/14.jpg" alt="Image" class="img-fluid">
+            </figure>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-lg-7 mx-auto text-center">
+            <span class="subheading">Our Services</span>
+            <h2 class="heading"><strong class="text-primary">Health Services</strong> We Provided</h2>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-6 col-sm-6 col-md-6 col-lg-4 mb-4 mb-lg-4">
+            <a href="#" class="service-v1 text-center">
+              <span class="flaticon-stethoscope"></span>
+              <h3>General Surgery</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, ex!</p>
+            </a>
+          </div>
+          <div class="col-6 col-sm-6 col-md-6 col-lg-4 mb-4 mb-lg-4">
+            <a href="#" class="service-v1 text-center">
+              <span class="flaticon-telephone"></span>
+              <h3>Outpatient Services</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, ex!</p>
+            </a>
+          </div>
+          <div class="col-6 col-sm-6 col-md-6 col-lg-4 mb-4 mb-lg-4">
+            <a href="#" class="service-v1 text-center">
+              <span class="flaticon-sphygmomanometer"></span>
+              <h3>Respiratory Therapy</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, ex!</p>
+            </a>
+          </div>
+
+
+          <div class="col-6 col-sm-6 col-md-6 col-lg-4 mb-4 mb-lg-4">
+            <a href="#" class="service-v1 text-center">
+              <span class="flaticon-dna"></span>
+              <h3>Cardiac Clinic</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, ex!</p>
+            </a>
+          </div>
+          <div class="col-6 col-sm-6 col-md-6 col-lg-4 mb-4 mb-lg-4">
+            <a href="#" class="service-v1 text-center">
+              <span class="flaticon-thermometer"></span>
+              <h3>Laryngological Service</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, ex!</p>
+            </a>
+          </div>
+          <div class="col-6 col-sm-6 col-md-6 col-lg-4 mb-4 mb-lg-4">
+            <a href="#" class="service-v1 text-center">
+              <span class="flaticon-syringe"></span>
+              <h3>Respiratory Therapy</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, ex!</p>
+            </a>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    
+
+    
